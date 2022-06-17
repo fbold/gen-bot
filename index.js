b208c130148904e1292811ee835e8984c7add7c9
Vurak <vurak.of.sulcan@gmail.com>
2022-06-17 22:42:44 +0100

    added gen love command

--- index.js
+++ index.js
@@ -139,6 +139,14 @@ gen list quote     - la llista dels quotes d'en gen \`\`\``)
         "https://media.discordapp.net/attachments/427459241415147532/974783101588897852/gen.gif"
       )
     msg.channel.send({ embeds: [genEmbed] })
+  } else if (msgContent.startsWith("gen love")) {
+    const genEmbed = new MessageEmbed()
+      .setColor("#FFC0CB")
+      .setTitle("Jeje  :relaxed:")
+      .setImage(
+        "https://media.discordapp.net/attachments/507558260790919200/980564554985652274/InShot_20220529_211237857.jpg"
+      )
+    msg.channel.send({ embeds: [genEmbed] })
   } else if (msgContent.startsWith("gen forum")) {
     msg.channel.send(`
 <@&701195716806180875>
